7969846bc6500812864146dfbe64f179ca973a13
Shardul Narvekar <shardulknarvekar@gmail.com>
2026-07-05 08:19:17 +0900

    setup github and started from the very basics

---
+++ Learn the basics/Things to Know in any language/inputoutput.cpp
@@ -0,0 +1,32 @@
+/*#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Hi"<<"\n";
+    cout<<"Hi";
+    return 0;
+}
+*/
+
+/*#include <iostream>
+using namespace std;
+int main(){
+    int x, y, z;
+    cin >> x >> y >> z;
+    cout << "Value of x is: " << x <<endl;
+    cout << "Value of y is: " << y <<endl;
+    cout << "Value of z is: " << z <<endl;
+    return 0;
+}*/
+
+#include <bits/stdc++.h> //this syntax includes almost all the libraries at once, instead of adding them one by one
+using namespace std;
+int main(){
+    int x, y, z;
+    cin >> x >> y >> z;
+    cout << "Value of x is: " << x <<endl;
+    cout << "Value of y is: " << y <<endl;
+    cout << "Value of z is: " << z <<endl;
+    return 0;
+}
+// Used in competitive programming
+// Not recommended for production due to compile time overhead
